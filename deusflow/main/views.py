c2641aaa9ec5b53534dfdcf191da436fc56ec830
DeusFlow <deuswork@icloud.com>
2025-03-10 21:36:26 +0100

    Initial commit

---
+++ deusflow/main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def about(request):
+    return HttpResponse("About us")
