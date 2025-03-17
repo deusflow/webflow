b39d1e691127bd7ac9b446cb6f04916b8b42a7f0
DeusFlow <deuswork@icloud.com>
2025-03-17 23:18:45 +0100

    Linked the news tab to the database, now you can add news and it will be saved, also worked on the design and other GUI

--- deusflow/newsflow/urls.py
+++ deusflow/newsflow/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.newsflow_home, name='newsflow_home'),
-    path('create', views.create, name='create')
+    path('create', views.create, name='create'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='newsflow_detail')
 ]
--- deusflow/newsflow/views.py
+++ deusflow/newsflow/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticleForm
+from django.views.generic import DetailView
 
 # Create your views here.
 def newsflow_home(request):
     news = Articles.objects.all()
     return render(request,'newsflow/newsflow_home.html',{'newsflow':news})
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'newsflow/details_view.html'
+    context_object_name = 'article'
 
 def create(request):
     error = ''
